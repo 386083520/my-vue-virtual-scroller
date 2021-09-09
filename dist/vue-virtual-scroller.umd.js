5bb9951491cdf3f71f62dd63b4491447bd27a4a9
386083520 <386083520@qq.com>
2021-09-10 07:09:10 +0800

    rollup构建代码

---
+++ dist/vue-virtual-scroller.umd.js
@@ -0,0 +1,9 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}((function () { 'use strict';
+
+	console.log('gsd666');
+
+})));
+//# sourceMappingURL=vue-virtual-scroller.umd.js.map
