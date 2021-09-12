cefa4d11b4dabb80048f8a2eacbfdd4b9b1a1a96
386083520 <386083520@qq.com>
2021-09-12 12:55:16 +0800

    DynamicScroller引入

---
+++ docs-src/src/components/MyDemo4.vue
@@ -0,0 +1,51 @@
+<template>
+    <DynamicScroller
+            :items="items"
+            :min-item-size="54"
+            class="scroller"
+    >
+        <template v-slot="{ item, index, active }">
+            <DynamicScrollerItem
+                    :item="item"
+                    :active="active"
+                    :size-dependencies="[
+          item.name,
+        ]"
+                    :data-index="index"
+            >
+                <div class="avatar">
+                    <img
+                            :src="item.url"
+                            :key="item.url"
+                            alt="avatar"
+                            class="image"
+                    >
+                </div>
+                <div class="text">{{ item.name }}</div>
+            </DynamicScrollerItem>
+        </template>
+    </DynamicScroller>
+</template>
+
+<script>
+    let listTemp = []
+    let url = ''
+    for (let i = 0; i < 2000; i++) {
+        url = (i%2 === 0 ? 'https://avatars.githubusercontent.com/u/16269252?v=4' + '?' + Math.random() : 'https://avatars.githubusercontent.com/u/495429?v=4' + '?' + Math.random())
+        listTemp.push({name: 'gsd' + i, id: i,size: 20 + Math.random()*20, url: url})
+    }
+    export default {
+        name: "MyDemo4",
+        data() {
+            return {
+                items: listTemp
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .scroller {
+        height: 100%;
+    }
+</style>
--- docs-src/src/router.js
+++ docs-src/src/router.js
@@ -6,12 +6,14 @@ import Home from './components/Home.vue'
 import MyDemo1 from './components/MyDemo1.vue'
 import MyDemo2 from './components/MyDemo2.vue'
 import MyDemo3 from './components/MyDemo3.vue'
+import MyDemo4 from './components/MyDemo4.vue'
 
 export default new VueRouter({
     routes: [
         { path: '/', name: 'home', component: Home },
         { path: '/mydemo1', name: 'mydemo1', component: MyDemo1 },
         { path: '/mydemo2', name: 'mydemo2', component: MyDemo2 },
-        { path: '/mydemo3', name: 'mydemo3', component: MyDemo3 }
+        { path: '/mydemo3', name: 'mydemo3', component: MyDemo3 },
+        { path: '/mydemo4', name: 'mydemo4', component: MyDemo4 }
     ],
 })
